de4ef6b4e31e063cd1c480885a77b3c8a08dbe3a
tamcy <tamcy@users.noreply.github.com>
2022-09-26 15:26:31 +0800

    initial commit (v1.006)

---
+++ README.md
@@ -0,0 +1,10 @@
+Chiron Sung HK: a typeface for Traditional Chinese users
+=======================
+
+Chiron Sung HK (昭源宋體) is a Traditional Chinese serif typeface based on [Adobe](https://adobe.com/)’s [Source Han Serif](https://github.com/adobe-fonts/source-han-serif) (a.k.a. [Google](https://www.google.com)’s [Noto Sans CJK](https://github.com/googlefonts/noto-cjk/)). The font aims at providing a modern, region-agnostic glyph set adopting the “modern” glyph style that is similar to the prevailing, usually commercial, typefaces in the Traditional Chinese regions.
+
+**IMPORTANT: This repository is specifically built for the [Google Fonts](https://fonts.google.com/) service. For a more complete overview, documentation and font configuration please visit the [original repository](https://github.com/chiron-fonts/chiron-sung-hk) or the [official website](https://chiron-fonts.github.io/) (in Chinese only).**
+
+## License
+
+Chiron Sung HK is available under the OFL-1.1 License and can be used free of charge, for both commercial and non-commercial purposes. See [OFL.txt](OFL.txt) for details.
---
+++ source/build.py
@@ -0,0 +1,63 @@
+import sys
+import ufoLib2
+import ufo2ft
+import subprocess
+from ufo2ft.fontInfoData import getAttrWithFallback
+from fontTools.designspaceLib import DesignSpaceDocument
+from fontTools.ttLib import newTable
+from fontTools.misc.roundTools import otRound
+
+def open_ufo(path):
+    return ufoLib2.Font.open(path)
+
+class CJKOutlineTTFCompiler(ufo2ft.outlineCompiler.OutlineTTFCompiler):
+
+    @staticmethod
+    def makeMissingRequiredGlyphs(font, glyphSet, sfntVersion, notdefGlyph=None):
+        # Make sure we don't add .notdef
+        return
+
+    def setupTable_post(self):
+        # post table must be format 3
+        if "post" not in self.tables:
+            return
+
+        self.otf["post"] = post = newTable("post")
+        font = self.ufo
+        post.formatType = 3.0
+        # italic angle
+        italicAngle = getAttrWithFallback(font.info, "italicAngle")
+        post.italicAngle = italicAngle
+        # underline
+        underlinePosition = getAttrWithFallback(
+            font.info, "postscriptUnderlinePosition"
+        )
+        post.underlinePosition = otRound(underlinePosition)
+        underlineThickness = getAttrWithFallback(
+            font.info, "postscriptUnderlineThickness"
+        )
+        post.underlineThickness = otRound(underlineThickness)
+        post.isFixedPitch = getAttrWithFallback(font.info, "postscriptIsFixedPitch")
+        # misc
+        post.minMemType42 = 0
+        post.maxMemType42 = 0
+        post.minMemType1 = 0
+        post.maxMemType1 = 0
+
+def build(designspace, filename, tableDir):
+    designspace = DesignSpaceDocument.fromfile(designspace)
+    designspace.loadSourceFonts(opener=open_ufo)
+
+    font = ufo2ft.compileVariableTTF(
+            designspace,
+            outlineCompilerClass=CJKOutlineTTFCompiler,
+            inplace=True,
+        )
+
+    dest = "../fonts/variable/" + filename
+    font.save(dest)
+
+    subprocess.run(["sfntedit", "-a", "cmap={0}/.tb_cmap,GDEF={0}/.tb_GDEF,GPOS={0}/.tb_GPOS,GSUB={0}/.tb_GSUB,DSIG={0}/.tb_DSIG,name={0}/.tb_name,OS/2={0}/.tb_OS2,hhea={0}/.tb_hhea,post={0}/.tb_post,STAT={0}/.tb_STAT,fvar={0}/.tb_fvar".format(tableDir), dest])
+
+build("ChironSungHK.designspace", "ChironSungHK-[wght].ttf", "tables")
+build("ChironSungHK-Italic.designspace", "ChironSungHK-Italic-[wght].ttf", "tables-italic")
